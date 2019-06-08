5021a6fb0a48a6335e39f3554a4f25d74b6f701a
Heverton <hevertonluiz@hotmail.com>
2019-06-08 11:34:33 -0300

    Commit imobiliaria 1.4

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use App\Entity\Corretor;
 use App\Forms\UsuarioType;
 use App\Entity\Usuario;
+use App\Service\UsuarioService;
 use phpDocumentor\Reflection\DocBlock\Tags\Throws;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
@@ -21,10 +22,9 @@ class UsuarioController extends AbstractController
 {
 
     /**
-     *@IsGranted("ROLE_ADMIN")
      * @Route("/usuario", name="usuario_novo")
      */
-    public function cadastroUsuario(Request $request)
+    public function cadastroUsuario(Request $request, UsuarioService $usuarioService)
     {
         $usuario = new Usuario();
         $form = $this->createForm(UsuarioType::class, $usuario);
@@ -32,11 +32,8 @@ class UsuarioController extends AbstractController
 
         if ($form->isSubmitted()) {
             $usuario = $form->getData();
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($usuario);
-            $em->flush();
-
-            return $this->redirectToRoute('index');
+            $usuarioService->salvar($usuario);
+            return $this->redirectToRoute('listar_usuarios');
         }
 
         return $this->render('usuario_cadastro.html.twig', [
