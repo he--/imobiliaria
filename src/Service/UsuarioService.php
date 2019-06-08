97b36d31a4e3e3fe59407a27ea3281d66391453c
ENGENHARIA E QUALIDADE DE SOFTWARE <eqs@unipe.local>
2019-06-08 11:35:13 -0300

    Adicionando o Service e Repository de Usuario

---
+++ src/Service/UsuarioService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Usuario;
+use App\Repository\UsuarioRepository;
+
+class UsuarioService
+{
+
+    /**
+     * @var UsuarioRepository
+     */
+    private $usuarioRepository;
+
+    public function __construct(UsuarioRepository $usuarioRepository)
+    {
+        $this->usuarioRepository = $usuarioRepository;
+    }
+
+    public function salvar(Usuario $usuario)
+    {
+        $this->usuarioRepository->salvar($usuario);
+    }
+}
