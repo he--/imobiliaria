53b0862d2ac398fed7b4fade06ab96cd0e514630
Adriano Barroso Silvestre <46399147+silvestrejp@users.noreply.github.com>
2019-06-08 18:10:07 -0300

    Commit no Projeto - Sábado pela tarde

---
+++ Controller/ImovelController.php
@@ -0,0 +1,86 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Imovel;
+use App\Forms\ImovelType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ImovelController extends AbstractController
+{
+
+    /**
+     * @Route("/imovel/cadastro", name="cadasto_imovel")
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function cadastroImovel(Request $request)
+    {
+
+        $imovel = new Imovel();
+        $form = $this->createForm(ImovelType::class, $imovel);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $imovel = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($imovel);
+            $em->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('imovel_cadastro.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
+
+    /**
+     * @Route("/imovel/listar", name="listar_imoveis")
+     */
+    public function listarImoveis()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $imoveis = $em->getRepository(Imovel::class)->findAll();
+
+        return $this->render('listar_imoveis.html.twig', [
+            'imoveis' => $imoveis
+        ]);
+    }
+
+    /**
+     * @Route("/imovel/portifolios", name="listar_portifolios")
+     */
+    public function imoveisPortifolios()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $imoveis = $em->getRepository(Imovel::class)->findAll();
+
+        return $this->render('listar_portifolios.html.twig', [
+            'imoveis' => $imoveis
+        ]);
+    }
+
+    /**
+     * @Route("/imovel/visualizar/{id}", name="imovel_visualizar")
+     */
+    public function imovelVisualizar(Request $request)
+    {
+        $id = $request->get('id');
+        $em = $this->getDoctrine()->getManager();
+        $imovel = $em->getRepository(Imovel::class)->find($id);
+
+        return $this->render('imovel_visualizar.html.twig', [
+            'imovel' => $imovel
+        ]);
+    }
+
+
+
+}
