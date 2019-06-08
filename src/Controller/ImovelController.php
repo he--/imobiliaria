c6c873f15921906832d199b00a807eef8d1b2f5f
rafaelcordeiro <rafaeldavi54@gmail.com>
2019-06-08 11:36:54 -0300

    segunda pratica

--- src/Controller/ImovelController.php
+++ src/Controller/ImovelController.php
@@ -1,86 +1,37 @@
 <?php
 
-
 namespace App\Controller;
 
-
 use App\Entity\Imovel;
-use App\Forms\ImovelType;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * Class ImovelController
+ * @package App\Controller
+ */
 class ImovelController extends AbstractController
 {
-
     /**
-     * @Route("/imovel/cadastro", name="cadasto_imovel")
-     *
+     * @Route("imovel/cadastro", name="imovel_cadastro")
      * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
     public function cadastroImovel(Request $request)
     {
 
-        $imovel = new Imovel();
-        $form = $this->createForm(ImovelType::class, $imovel);
-        $form->handleRequest($request);
+       $imovel = new Imovel();
+       $imovel->setStatus("novo");
+       $imovel->setCaracteristicas("limpa");
+       $imovel->setObservacao( "observacao");
+       $imovel->setCadastro(new \DateTime());
+       $imovel->setImovel("apartamento");
 
-        if ($form->isSubmitted()) {
-            $imovel = $form->getData();
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($imovel);
-            $em->flush();
-
-            return $this->redirectToRoute('index');
-        }
-
-        return $this->render('imovel_cadastro.html.twig', [
-            'form' => $form->createView()
-        ]);
+        $em = $this->$this->getDoctrine()->getManager();
+        $em->persist($imovel);
+        $em->flush();
 
     }
-
-    /**
-     * @Route("/imovel/listar", name="listar_imoveis")
-     */
-    public function listarImoveis()
-    {
-        $em = $this->getDoctrine()->getManager();
-        $imoveis = $em->getRepository(Imovel::class)->findAll();
-
-        return $this->render('listar_imoveis.html.twig', [
-            'imoveis' => $imoveis
-        ]);
-    }
-
-    /**
-     * @Route("/imovel/portifolios", name="listar_portifolios")
-     */
-    public function imoveisPortifolios()
-    {
-        $em = $this->getDoctrine()->getManager();
-        $imoveis = $em->getRepository(Imovel::class)->findAll();
-
-        return $this->render('listar_portifolios.html.twig', [
-            'imoveis' => $imoveis
-        ]);
-    }
-
-    /**
-     * @Route("/imovel/visualizar/{id}", name="imovel_visualizar")
-     */
-    public function imovelVisualizar(Request $request)
-    {
-        $id = $request->get('id');
-        $em = $this->getDoctrine()->getManager();
-        $imovel = $em->getRepository(Imovel::class)->find($id);
-
-        return $this->render('imovel_visualizar.html.twig', [
-            'imovel' => $imovel
-        ]);
-    }
-
-
 
 }
